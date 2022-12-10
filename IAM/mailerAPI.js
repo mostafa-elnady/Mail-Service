c9d290b5d81c31cfe61d5fcb83345c573d461914
mostafa-elnady <61089586+mostafa-elnady@users.noreply.github.com>
2022-12-10 13:04:21 +0200

    sending after time if does't complete registration
    you can sent [emailAddress, emailSubject, emailHtml , isCompleted] and isCompleted is boolean true or false if true  api does not sent email and if false api send email after time

--- IAM/mailerAPI.js
+++ IAM/mailerAPI.js
@@ -29,8 +29,38 @@ function sendMail(emailAddress, emailSubject, emailHtml) {
 
 }
 
+////****** SENDING EMAIL FUNCTION  after 10 minite if user does not complete registeration prosses ******
+function sendMailAfterTime(emailAddress, emailSubject, emailHtml , isCompleted) {
+    const data = {
+        mail: {
+            to: [
+                emailAddress
+            ],
+            cc: "",
+            bcc: "",
+            subject: emailSubject,
+            html: emailHtml,
+        },
+        isCompleted: isCompleted,
+       
+    }
+
+// API DOMAIN NEEDS TO BE CHANGED TO RENDER DOMAIN WHEN USED BY THE IAM GROUP
+    const axios = require('axios').default;
+    axios.post('https://mail-service-69zm.onrender.com/api/mail/sendeEmailAfterTime', data)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+
+}
+
 module.exports = {
     sendMail,
+    sendMailAfterTime
 };
 //end of file: mailerAPI.js
 
